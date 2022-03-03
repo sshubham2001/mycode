cd7802ff47e8737e3f80e8fd84a3b45a4aa77590
Nithya-B-au17 <nithya@geekaspire.com>
2022-03-03 10:56:44 +0530

    Change old Notification to New Notification

--- src/store/actions/admin.js
+++ src/store/actions/admin.js
@@ -1,61 +1,37 @@
-import axios from "axios";
-import { store } from "react-notifications-component";
+import axios from 'axios'
+import { message as notify } from 'antd'
 
 export const generateMontlyReport = (dates, history) => async (dispatch) => {
   try {
-    const token = localStorage && localStorage.getItem("token");
-    console.log(token);
+    const token = localStorage && localStorage.getItem('token')
+    console.log(token)
     const config = {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         authorization: `Bearer ${token}`,
       },
-    };
+    }
 
     const res = await axios.post(
       `${process.env.REACT_APP_DATABASEURL}/admin/generate-report`,
       { dateOne: dates[0], dateTwo: dates[1] },
       config
-    );
-    res.statusText && dispatch({ type: "STOP_LOADER" });
+    )
+    res.statusText && dispatch({ type: 'STOP_LOADER' })
     const {
       data: { status, message, data },
-    } = res;
+    } = res
     status &&
       dispatch({
-        type: "FETCH_MONTHLY_REPORT",
+        type: 'FETCH_MONTHLY_REPORT',
         payload: data,
-      });
+      })
     if (status) {
-      store.addNotification({
-        title: "Success!",
-        message: message,
-        type: "success",
-        insert: "top",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-          duration: 2000,
-          onScreen: true,
-        },
-      });
+      notify.success(message)
     } else {
-      store.addNotification({
-        title: "Failed!",
-        message: message,
-        type: "danger",
-        insert: "top",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-          duration: 2000,
-          onScreen: true,
-        },
-      });
+      notify.error(message)
     }
   } catch (error) {
-    console.log("🤞HurraY ERROR", error);
+    console.log('🤞HurraY ERROR', error)
   }
-};
+}
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,134 +1,88 @@
-import axios from "axios";
-import { store } from "react-notifications-component";
+import axios from 'axios'
+import { message as notify } from 'antd'
 
 export const viewOrder = (order) => async (dispatch) => {
   dispatch({
-    type: "VIEW_ORDER",
+    type: 'VIEW_ORDER',
     payload: order,
-  });
-};
+  })
+}
 
 export const fetchOrder = (history) => async (dispatch) => {
   try {
-    const token = localStorage && localStorage.getItem("token");
-    console.log(token);
+    const token = localStorage && localStorage.getItem('token')
+    console.log(token)
     const config = {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         authorization: `Bearer ${token}`,
       },
-    };
+    }
     // Fetching Orders
     const fetchOrder = await axios.post(
       `${process.env.REACT_APP_DATABASEURL}/admin/fetch-order`,
       {},
       config
-    );
+    )
     const {
       data: { status: status, data: orders, message },
-    } = fetchOrder;
+    } = fetchOrder
     // console.log(orders);
     if (status) {
       dispatch({
-        type: "FETCH_ORDERS",
+        type: 'FETCH_ORDERS',
         payload: orders,
-      });
+      })
       dispatch({
-        type: "STOP_LOADER",
-      });
+        type: 'STOP_LOADER',
+      })
     }
-    !status &&
-      store.addNotification({
-        title: "Failed!",
-        message: message,
-        type: "danger",
-        insert: "top",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-          duration: 2000,
-          onScreen: true,
-        },
-      });
-    !status && localStorage.removeItem("token");
-    !status && localStorage.removeItem("isAuth");
-    !status && history.push("/login");
+    !status && notify.error(message)
+
+    !status && localStorage.removeItem('token')
+    !status && localStorage.removeItem('isAuth')
+    !status && history.push('/login')
   } catch (error) {
-    console.log(error);
+    console.log(error)
     // localStorage.removeItem("token");
     // localStorage.removeItem("isAuth");
-    store.addNotification({
-      title: "Failed!",
-      message: "Session timed out. Please login again!",
-      type: "danger",
-      insert: "top",
-      container: "top-right",
-      animationIn: ["animate__animated", "animate__fadeIn"],
-      animationOut: ["animate__animated", "animate__fadeOut"],
-      dismiss: {
-        duration: 2000,
-        onScreen: true,
-      },
-    });
+    notify.error('Session timed out. Please login again!')
+
     // history.push("/login");
     // window.location.reload();
   }
-};
+}
 export const updateOrder = (couponDetails, history) => async (dispatch) => {
   try {
-    const token = localStorage && localStorage.getItem("token");
-    console.log(token);
+    const token = localStorage && localStorage.getItem('token')
+    console.log(token)
     const config = {
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         authorization: `Bearer ${token}`,
       },
-    };
+    }
     const res = await axios.post(
       `${process.env.REACT_APP_DATABASEURL}/order/update-order-status`,
       couponDetails,
       config
-    );
+    )
     const {
       data: { status, message, data },
-    } = res;
+    } = res
     status &&
       dispatch({
-        type: "FETCH_ORDERS",
+        type: 'FETCH_ORDERS',
         payload: data,
-      });
+      })
     if (status) {
-      store.addNotification({
-        title: "Success!",
-        message: message,
-        type: "success",
-        insert: "top",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-          duration: 2000,
-          onScreen: true,
-        },
-      });
-      history.push("/orders");
+      notify.success(message)
+
+      history.push('/orders')
     } else {
-      store.addNotification({
-        title: "Failed!",
-        message: message,
-        type: "danger",
-        insert: "top",
-        container: "top-right",
-        animationIn: ["animate__animated", "animate__fadeIn"],
-        animationOut: ["animate__animated", "animate__fadeOut"],
-        dismiss: {
-          duration: 2000,
-          onScreen: true,
-        },
-      });
+      notify.error(message)
     }
   } catch (error) {
-    console.log("🤞HurraY ERROR", error);
+    console.log('🤞HurraY ERROR', error)
   }
-};
+}
