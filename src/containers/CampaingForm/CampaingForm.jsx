9b3d985f9f2ad74d4d1fb1c67dac0b452decebc7
Nithya-B-au17 <nithya@geekaspire.com>
2022-03-16 12:44:51 +0530

    Changed maxAmount to minAmount

--- src/containers/CampaingForm/CampaingForm.jsx
+++ src/containers/CampaingForm/CampaingForm.jsx
@@ -72,7 +72,7 @@ const AddCampaing = (props) => {
       discount: percent,
       count,
       code,
-      maxAmount: amount,
+      minAmount: amount,
       category: category[0]?.value,
       media: image,
       email: user,
